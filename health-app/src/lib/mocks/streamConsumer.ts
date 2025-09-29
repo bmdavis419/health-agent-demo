f7e55d4d9e32ab020f013474409d21339a714a09
Ben Davis <davis.benjamin41902@gmail.com>
2025-09-28 17:52:34 -0700

    better mock

--- health-app/src/lib/mocks/streamConsumer.ts
+++ health-app/src/lib/mocks/streamConsumer.ts
@@ -1,9 +1,15 @@
+type Stream<Chunk> = {
+	__chunkType: Chunk;
+	// all the other stuff u would need idk that's ur problem
+};
+
 type OnCompleteCallback = () => void;
 type OnErrorCallback = (error: Error) => void;
 type OnChunkCallback<Chunk> = (chunk: Chunk) => void;
 type OnStartCallback = () => void;
 
 type MockStreamConsumer<Chunk> = (
+	stream: Stream<Chunk>,
 	streamId: string,
 	args: {
 		onComplete: OnCompleteCallback;
